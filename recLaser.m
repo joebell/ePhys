67f4d916168fe39a22bc50b5b9f3236bca3f5b13
Joe Bell <joe.bell@gmail.com>
2014-01-13 15:39:00 -0500

    Fixed recLaser.m

--- recLaser.m
+++ recLaser.m
@@ -1,8 +1,11 @@
-function recLaser(expNum, stimulus)
+function data = recLaser(expNum, stimulus)
+
+    import jDAQmx.*;
 
     rigConfig();
+    data.inputSampleRate = 10000;
       
-    %% Get telegraph data
+    %% Get telegraph data, fail if not set to I=0
     [mode, Vgain, Igain] = getTelegraphs();
     if (~strcmp(mode, 'I=0'))
         disp('Set Amplifier to ''I = 0'' mode and try again.');
@@ -12,47 +15,49 @@ function recLaser(expNum, stimulus)
     data.voltageScale = Vgain;
     
     %% Setup AI
-    AI.analogInput(devName);
-    AI.addChannel(channelList);  
-    AI.setSampleRate(sampleRate,NsampPerChan);
+    AI = analogInput(devName);
+    AI.addChannel(recordingInputs);
+    stimLength = size(stimulus.waveform,1)./stimulus.outputSampleRate;
+    NsampPerChan = round(stimLength*data.inputSampleRate);
+    AI.setSampleRate(data.inputSampleRate,NsampPerChan);
     
     %% Setup AO
-    AO.analogOutput(devName);
-    AO.addChannel(channelList);
-    AO.setSampleRate(sampleRate,NsampPerChan);
-    AO.putData(someData);                          % NsampPerChan x length(channelList)
-    AO.start();     % AO is triggered off of AI
+    AO = analogOutput(devName);
+    AO.addChannel(stimulus.channels);
+    AO.setSampleRate(stimulus.outputSampleRate,size(stimulus.waveform,1));
+    AO.putData(stimulus.waveform);  % NsampPerChan x length(channelList)
+    AO.start();                     % AO is triggered off of AI
     
-
-    %% Put data to DAC
-    putdata(AO, stimulus.waveform);
-    start([AI AO]);
-    trigger([AI AO]);
+    %% Start acquisition
+    AI.start();
     
-    % wait for playback/recording to finish
+    %% Wait for playback/recording to finish
     fprintf('     Recording.');
-    totalSamples = length(stimulus.waveform);
-    inputSamples = round(totalSamples*inputSampleRate*overSample/outputSampleRate);
-    nsampout = AO.SamplesOutput;
-    while (nsampout < totalSamples)
-        nsampout = AO.SamplesOutput;
-        pause(1);
+    tic();
+    while (toc < stimLength)
+        pause(.25);
         fprintf('.');
     end 
     disp('.');
-    % stop playback
-    stop([AI AO]);
+    AI.wait();
     disp('     Stopped recording.');
     
     %% Get data from DAC
-    dataStream = getdata(AI,inputSamples);
-    V = 1000*dataStream(:,find(inputList == axoScaledInput))/data.voltageScale;
-    data.V = decimate(V,overSample); 
+    dataStream = AI.getData;
+    data.V = 1000*dataStream(:,find(recordingInputs == scaledInput))/data.voltageScale;
     stimulus.waveform = [];
     data.stimulus = stimulus;
-    data.sampleRate = inputSampleRate;
+    data.sampleRate = data.inputSampleRate;
     
     %% Write data to disk
-    fileName = dataStorage(expNum); 
-    save(fileName,'data');
-    disp(['Wrote to: ',fileName]);
+    if (expNum > 0)
+        fileName = dataStorage(expNum); 
+        save(fileName,'data');
+        disp(['Wrote to: ',fileName]);
+    end
+    
+    %% Stop and clear objects
+    AI.stop();
+    AI.clear();
+    AO.stop();
+    AO.clear();
